abeb69842c6cd039b2709774ac6d3821b9aaa183
Shardul1908 <sdharmad@usc.edu>
2024-04-09 19:31:44 -0700

    Deployment Config

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,5 +1,5 @@
 import { io } from 'socket.io-client';
-const URL = 'http://localhost:9000';
+const URL = 'https://tictactoe-server-f0f96fe5e12f.herokuapp.com/';
 export const socket = io(URL, {
     autoConnect: false
 });
